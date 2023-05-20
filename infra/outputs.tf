229e3a2b026e5189296767f39d7bd3c82106e639
Mark Demo <mark@londonappdeveloper.com>
2023-05-20 16:14:14 +0100

    added eks

---
+++ infra/outputs.tf
@@ -0,0 +1,9 @@
+output "cluster_name" {
+  description = "Name of EKS cluster in AWS"
+  value       = module.eks.cluster_name
+}
+
+output "region" {
+  description = "AWS region"
+  value       = var.region
+}
