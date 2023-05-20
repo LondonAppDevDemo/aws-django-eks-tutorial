992d48c2170680b7143ce0a1242296340d50886e
Mark Demo <mark@londonappdeveloper.com>
2023-05-20 15:50:05 +0100

    Added RDS

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,3 +7,8 @@ variable "prefix" {
   description = "Prefix to be assigned to resources"
   default     = "django-k8s"
 }
+
+variable "db_password" {
+  description = "Password for the RDS database instance."
+  default     = "samplepassword123"
+}
