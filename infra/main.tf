9cc545fc55ef19839a0dd4262a14962723d0df92
Mark Demo <mark@londonappdeveloper.com>
2023-05-20 15:34:48 +0100

    Added VPC

--- infra/main.tf
+++ infra/main.tf
@@ -11,3 +11,5 @@ terraform {
 provider "aws" {
   region = var.region
 }
+
+data "aws_availability_zones" "available" {}
--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,8 @@ variable "region" {
   description = "AWS region to deploy resources to"
   default     = "eu-west-2"
 }
+
+variable "prefix" {
+  description = "Prefix to be assigned to resources"
+  default     = "django-k8s"
+}
